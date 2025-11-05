27f4b8080d8d6c8fbf7e50c5102971aea7568e40
gabeamv <gabrielvillanueva11155@gmail.com>
2025-11-04 20:53:26 -0800

    ch5

--- Try_It_Yourself/Try_It_Yourself.sql
+++ Try_It_Yourself/Try_It_Yourself.sql
@@ -75,3 +75,47 @@ ORDER BY salary DESC;
 -- order by first name.
 
 --3. An error will occur trying to convert '4//2021' to timestamp.
+
+-- Chapter 5
+
+--1. 
+COPY actors
+FROM 'path\.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER ':', QUOTE '#');
+
+--2.
+COPY (
+	SELECT county_name, state_name, births_2019
+	FROM us_counties_pop_est_2019
+	ORDER BY births_2019 DESC
+	LIMIT 20
+)
+TO 'C:\Users\gamvr\Desktop\Programming\Repos\practical-sql-2\Chapter_05\us_counties_births_export.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+county_name|state_name|births_2019
+Los Angeles County|California|112963
+Harris County|Texas|66937
+Cook County|Illinois|61822
+Maricopa County|Arizona|52722
+San Diego County|California|40603
+Dallas County|Texas|38374
+Kings County|New York|37786
+Orange County|California|36406
+Miami-Dade County|Florida|30789
+Riverside County|California|29482
+San Bernardino County|California|29341
+Tarrant County|Texas|27512
+Queens County|New York|27453
+Clark County|Nevada|27048
+Bexar County|Texas|26736
+King County|Washington|25188
+Wayne County|Michigan|22766
+Broward County|Florida|21829
+Santa Clara County|California|21713
+Philadelphia County|Pennsylvania|20924
+
+--3. Will numeric(3,8) work for 17519.668, etc.
+-- No, because 3 is the max number for digits and 8 is the allocated digits for decimal.
+-- the given number is too big. Precision must be larger than scale.
+-- numeric(8,3) should be used instead of what was given.
